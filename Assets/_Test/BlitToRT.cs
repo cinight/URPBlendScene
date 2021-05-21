6802ba298475bf02af09c53927eb275f4e401999
MingWai Chan <mingwai@unity3d.com>
2021-05-21 17:26:37 +0200

    update blit script

--- Assets/_Test/BlitToRT.cs
+++ Assets/_Test/BlitToRT.cs
@@ -12,7 +12,13 @@ public class BlitToRT : ScriptableRendererFeature
     public string RTname;
     public Material material;
     public bool isDepth;
-    
+
+    [Header("Clearing options")]
+    public RTClearFlags clearFlag;
+    public Color clearColor;
+    public float clearDepth;
+    public uint clearStencil;
+
 	public BlitToRT()
 	{
 	}
@@ -23,7 +29,7 @@ public class BlitToRT : ScriptableRendererFeature
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {      
-        var pass = new BlitToRTPass(evt,material,RTname,isDepth);
+        var pass = new BlitToRTPass(evt,material,RTname,isDepth,clearFlag,clearColor,clearDepth,clearStencil);
         renderer.EnqueuePass(pass);
     }
 
@@ -35,30 +41,50 @@ public class BlitToRT : ScriptableRendererFeature
         private Material material;
         private bool isDepth;
 
-        public BlitToRTPass(RenderPassEvent evt, Material mat, string name, bool d)
+        private RTClearFlags clear_flag;
+        private Color clear_color;
+        private float clear_depth;
+        private uint clear_stencil;
+
+        //private int tempRTid = Shader.PropertyToID("TempRTForClear");
+       // private RenderTargetIdentifier tempRT;
+
+        public BlitToRTPass(RenderPassEvent evt, Material mat, string name, bool d, RTClearFlags clearFlag, Color clearColor, float clearDepth, uint clearStencil)
         {
+            this.clear_flag = clearFlag;
+            this.clear_color = clearColor;
+            this.clear_depth = clearDepth;
+            this.clear_stencil = clearStencil;
+
             this.isDepth = d;
             this.material = mat;
             this.RTname = name;
             this.renderPassEvent = evt;
         }
 
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescripor)
+        {
+            //tempRT = new RenderTargetIdentifier(tempRTid);
+            //cmd.GetTemporaryRT(tempRTid, cameraTextureDescripor);
+        }
+
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            if( material == null) return;
-
             var camera = renderingData.cameraData.camera;
 			CommandBuffer cmd = CommandBufferPool.Get(camera.name + " - BlitToRTPass - " + RTname);
-            if(isDepth)
-            {
-                cmd.Blit( null , renderingData.cameraData.renderer.cameraDepthTarget , material );
-                if(RTname != "") cmd.SetGlobalTexture(RTname,renderingData.cameraData.renderer.cameraDepthTarget);
-            }
-            else
-            {
-                cmd.Blit( null , renderingData.cameraData.renderer.cameraColorTarget , material );
-                if(RTname != "") cmd.SetGlobalTexture(RTname,renderingData.cameraData.renderer.cameraColorTarget);
-            }
+
+            RenderTargetIdentifier RT = renderingData.cameraData.renderer.cameraColorTarget;
+            if(isDepth) RT = renderingData.cameraData.renderer.cameraDepthTarget;
+
+            //Clearing Render Target
+            cmd.SetRenderTarget(RT);
+            cmd.ClearRenderTarget(clear_flag,clear_color,clear_depth,clear_stencil);
+            //cmd.Blit(tempRT,RT);
+            //cmd.ReleaseTemporaryRT(tempRTid);
+
+            if(material != null) cmd.Blit( null , RT , material );
+            if(RTname != "") cmd.SetGlobalTexture(RTname,RT);
+
             context.ExecuteCommandBuffer(cmd);
 			CommandBufferPool.Release(cmd);
 		}
